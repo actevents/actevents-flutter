40ed4ffa85c2a94ff4398c8f43e6687a32fe5568
maSchoeller <git@marvinschoeller.de>
2021-04-29 10:30:49 +0200

    [dev]: temp

--- actevents/lib/main.dart
+++ actevents/lib/main.dart
@@ -6,8 +6,6 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(home: MyApp()));
 
 class MyApp extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
